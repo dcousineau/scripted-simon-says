1cd00aa526ecf1352a8e8ae02933b09f8431f2f1
Daniel Cousineau <dcousineau@gmail.com>
2015-05-14 17:51:38 -0400

    Better starter code for app.js

--- js/app.js
+++ js/app.js
@@ -3,3 +3,17 @@
 //of the game logic. You should not _need_ to edit simongame.js however if you
 //want to add extra features like more colors, etc edit the file to your hearts
 //content.
+var game;
+
+function newGame() {
+    game = new SimonGame(4);
+    
+    //Display answer for 2s
+}
+
+//Listen for user guesses (clicking a color button)
+
+//Clicking reset game should start a newGame();
+
+//We should start a new game immediately on load
+newGame();
